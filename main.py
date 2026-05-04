1a77d1b3bd93b35a6e46ccd78c908be163d5868f
CRufo10T <rufo.cg@obmontessori.edu.ph>
2026-05-04 08:07:52 +0800

    Refactor Wallahi function and update matplotlib logging
    Updated logging level for matplotlib and improved handling of absence input.

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-
 from pyscript import display, document
 import numpy as np
-import logging
+import matplotlib
+
+matplotlib.set_loglevel("error")
 import matplotlib.pyplot as plt
-logging.getLogger('Matplotlib').setLevel(logging.ERROR)
-plt.figure()
-plt.plot([0,1], [0,1])
-plt.close()
 
 days = np.array(['Mon','Tue','Wed','Thurs','Fri'])
-
 absences = np.zeros(5, dtype=int)
 
 def Wallahi(e):
     day = document.getElementById("DayOfTheWeek").value
     index = list(days).index(day)
 
-    value = int(document.getElementById("absences").value)
+    value = int(document.getElementById("absences").value or 0)
     absences[index] += value
 
     document.getElementById("output").innerHTML = ""
